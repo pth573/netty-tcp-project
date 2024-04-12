cff688a49fb897d92db705d08fa46989f3d59d90
pth573 <phamhang572003@gmail.com>
2024-04-12 11:50:51 +0700

    first commit

--- demonetty/src/main/resources/netty.sql
+++ demonetty/src/main/resources/netty.sql
@@ -15,3 +15,11 @@ CREATE TABLE t1 (
     sequence VARCHAR(255),
     receive_message VARCHAR(500)
 );
+
+--
+--Chat TCP using netty framework Can chat TCP continuously
+--
+--Run Entry.class
+--Run ClientMain.class
+--At ClientMain.class, you can using readTxt or readFile, if you chat TCP, using readTxt and scan String terminal (Can scan continuously)
+--In ServerHandler.class at channelRead0: you can scan String and send to Client
